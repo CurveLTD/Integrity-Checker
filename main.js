d608a2ed07b5243f1554992f5baebe90961d8a9c
Pettless <30359999+Nerveyyyy@users.noreply.github.com>
2021-01-02 15:56:35 +0000

    Removed test file

--- main.js
+++
@@ -1,5 +0,0 @@
-let integrity = require("./dist/index");
-
-let isValid = integrity.hashSaveDir("./test", "696260a18072d97c7a7b3fed7119a4482f4835ac13be2d2db11707d7445ad783", "280c5f857d762e467d9015f20f115d74");
-
-console.log(isValid);
